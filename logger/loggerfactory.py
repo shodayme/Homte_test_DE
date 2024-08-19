2c0daa0085ea8e927348fdc7ac2839939ddc7f69
mehdi <mehdi@datagram.ai>
2024-08-19 14:30:49 +0100

    logger update

--- logger/loggerfactory.py
+++ logger/loggerfactory.py
@@ -1,22 +1,5 @@
 import logging
 
-'''class LoggerFactory:
-    def __init__(self, log_file_path):
-        logging.basicConfig(
-            format="%(asctime)s - %(levelname)s - %(message)s",
-            datefmt="[%m/%d/%Y %H:%M:%S]",
-            level=logging.INFO,
-            handlers=[
-                logging.FileHandler(log_file_path),  # File output
-                logging.StreamHandler()  # Console output
-            ]
-        )
-        self.logger = logging.getLogger(log_file_path)
-
-
-    def get_logger(self):
-        return self.logger
-'''
 
 class LoggerFactory:
     def __init__(self):
